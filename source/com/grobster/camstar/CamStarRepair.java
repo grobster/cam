936a65f46a4ef22e282de2b43e63cd36e9ade7a8
grobster <jquarles@grobster.com>
2014-11-07 21:18:22 -0500

    Removed the reference to ConsentRepairInterface and removed the constructor that took ConsentRepairInterface as argument

--- source/com/grobster/camstar/CamStarRepair.java
+++ source/com/grobster/camstar/CamStarRepair.java
@@ -3,11 +3,6 @@ package com.grobster.camstar;
 import java.nio.file.Path;
 
 public class CamStarRepair implements RenameBehavior {
-	private ConsentRepairInterface consentRepair;
-	
-	public CamStarRepair(ConsentRepairInterface consentRepair) {
-		this.consentRepair = consentRepair;
-	}
 	
 	public void renameFiles(Path path) {
 		
