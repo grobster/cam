f8a7d0cc108dd2802187adf10a313c94fd1fb564
grobster <jquarles@grobster.com>
2014-11-07 21:36:28 -0500

    organized methods in ConsentRepairInterface

--- source/com/grobster/camstar/ConsentRepairInterface.java
+++ source/com/grobster/camstar/ConsentRepairInterface.java
@@ -5,15 +5,15 @@ import java.nio.file.Path;
 public interface ConsentRepairInterface {
 	public void renameFiles(Path path);
 	public void setFullDirectoryPath(String path);
+	public void setFileEnding(String fileEnding);
+	public void setRenameBehavior(RenameBehavior renameBehavior); // new just added uses Strategy Pattern
 	public String getFullDirectoryPath();
 	public String getBaseDirectoryPath();
-	public int getNumberFilesRenamed();
-	public void setFileEnding(String fileEnding);
+	public String getOldFileName();
+	public String getNewFileName();
 	public String getFileEnding();
+	public int getNumberFilesRenamed();
 	public void registerObserver(FileRenamerObserver o);
 	public void removeObserver(FileRenamerObserver o);
 	public void notifyObservers();
-	public String getOldFileName();
-	public String getNewFileName();
-	public void setRenameBehavior(RenameBehavior renameBehavior); // new just added uses Strategy Pattern
 }
