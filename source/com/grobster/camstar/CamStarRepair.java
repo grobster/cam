cd5143df606d76d942327a4f3acbd7260392336d
grobster <jquarles@grobster.com>
2014-11-08 01:26:02 -0500

    updated the renameFiles method in CamStarRepair to take a String filter argument

--- source/com/grobster/camstar/CamStarRepair.java
+++ source/com/grobster/camstar/CamStarRepair.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 
 public class CamStarRepair implements RenameBehavior {
 	
-	public void renameFiles(Path path) {
+	public void renameFiles(Path path, String filter) {
 		
 	}
 }
