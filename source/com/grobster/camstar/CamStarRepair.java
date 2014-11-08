48e4e28ef25d2f0bbc2fb8f0afa2a5d21926872d
grobster <jquarles@grobster.com>
2014-11-08 05:53:09 -0500

    updated renameFiles method in CamStar class

--- source/com/grobster/camstar/CamStarRepair.java
+++ source/com/grobster/camstar/CamStarRepair.java
@@ -1,10 +1,14 @@
 package com.grobster.camstar;
 
 import java.nio.file.Path;
+import java.util.List;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 public class CamStarRepair implements RenameBehavior {
 	
 	public void renameFiles(Path path, String filter) {
-		
+
 	}
 }
