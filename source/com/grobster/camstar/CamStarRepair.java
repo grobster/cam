b2eb5a14a5c924fe5bcf3ed9d3cda937245e512c
grobster <jquarles@grobster.com>
2014-11-09 04:33:04 -0500

    added final logic to the renameFiles method in CamStarRepair class. added a String constant to the CamStarRepair class.

--- source/com/grobster/camstar/CamStarRepair.java
+++ source/com/grobster/camstar/CamStarRepair.java
@@ -5,13 +5,21 @@ import java.util.List;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import static java.nio.file.StandardCopyOption.*;
+import java.nio.file.Paths;
 
 public class CamStarRepair implements RenameBehavior {
+	public static final String SECOND_PART_PHOTO_CONSENT_NAME = " - Photo Consent";
 
 	public void renameFiles(Path path, String filter) {
 		try(DirectoryStream<Path> stream = Files.newDirectoryStream(path, "*" + filter)){ // filter based on file type
 			for (Path entry: stream) {
-				if (countPeriodsInFileName(entry) == 1) {
+				if (CamStarRepair.countPeriodsInFileName(entry) == 1) {
+					String[] tokens = entry.getFileName().toString().split("_");
+					if (tokens.length > 0) {
+						Path target = Paths.get(new StringBuilder(path.toString() + "\\" + tokens[0].trim() + CamStarRepair.SECOND_PART_PHOTO_CONSENT_NAME + filter).toString());
+						Files.move(entry, target, REPLACE_EXISTING, ATOMIC_MOVE); //rename the file
+					}
 				}
 			}
 		} catch (Exception ex) {
