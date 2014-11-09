e573ddb48da7bffd8c3647a9be73c9d604040136
grobster <jquarles@grobster.com>
2014-11-08 19:04:25 -0500

    changed countPeriodsInFileName method in CamStarRepair class from private to a public static int method

--- source/com/grobster/camstar/CamStarRepair.java
+++ source/com/grobster/camstar/CamStarRepair.java
@@ -7,23 +7,25 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 
 public class CamStarRepair implements RenameBehavior {
-	private int periodCount;
-	
+
 	public void renameFiles(Path path, String filter) {
 		try(DirectoryStream<Path> stream = Files.newDirectoryStream(path, "*" + filter)){ // filter based on file type
 			for (Path entry: stream) {
-				periodCount = 0;
+				if (countPeriodsInFileName(entry) == 1) {
+				}
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 	
-	private void countPeriodsInFileName(Path path) {
+	public static int countPeriodsInFileName(Path path) {
+		int count = 0;
 		for (int i = 0; i < path.getFileName().toString().length(); i++) {
 			if (path.getFileName().toString().charAt(i) == '.') {
-				periodCount++; // increase period count
+				count++; // increase period count
 			}
 		}
+		return count;
 	}
 }
